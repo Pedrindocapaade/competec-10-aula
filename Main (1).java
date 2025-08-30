3aa1ec38509467eeb9c8f987a2ac25ff01c8cf1e
Pedrindocapaade <pedroantunescarneiro@gmail.com>
2025-08-30 11:28:27 -0300

    Add files via upload

---
+++ Main (1).java
@@ -0,0 +1,19 @@
+public class Main
+{
+	public static void main(String[] args) {
+		System.out.println(calcularBaskara(10,-7,5));
+		
+	}
+	
+	public static float calcularImc(float altura,float peso){
+		    float imc = peso / (altura*altura);
+		    System.out.println("Seu imc é:"+imc);
+		    return imc;
+	}
+	
+	public static float calcularBaskara(float a,float b, float c){
+	    float bask = (b*b)-4*(a*c);
+	    System.out.println("O delta é igual a ="+ bask);
+	    return bask;
+	}
+}
